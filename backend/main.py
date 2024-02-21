55e29456a64cd973481369947990b989fc1a8fc7
Adejumo Ridwan Suleiman <adejumo999@gmail.com>
2024-02-21 14:24:33 +0100

    a

--- backend/main.py
+++ backend/main.py
@@ -25,4 +25,4 @@ async def get_prediction(weather_variable: str = Form(...), days: int = Form(...
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run("main:app", host="0.0.0.0",port=10000,reload=True)
